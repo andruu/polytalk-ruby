5183ee7fbb4176e7dd1008f6b765021fc4ab4a33
andruu <andru.weir@gmail.com>
2012-12-07 23:51:07 -0500

    updated to allow passing symbols as parameters

--- lib/polytalk/server.rb
+++ lib/polytalk/server.rb
@@ -14,22 +14,39 @@ module Polytalk
       loop do
         Thread.start(server.accept) do |connection|
           request = connection.recv(2048)
-          yield(connection, JSON.parse(request))
+          yield(connection, JSON.parse(request, symbolize_names: true))
         end
       end
     end
 
     def call(request)
       required_constant = nil
-      request['class'].split('::').each_with_index do |c, index|
+      request[:class].split('::').each_with_index do |c, index|
         if index == 0
           required_constant = Kernel.const_get(c)
         else
           required_constant = required_constant.const_get(c)
         end
       end
-      required_method = required_constant.method(request['method'])
-      required_method.call *request['arguments'].values
+
+      # Convert string prefixed with `:` to symbols
+      if request[:arguments].is_a? Hash
+        request[:arguments].each do |key, arg|
+          if arg.is_a? String
+            if arg.match(/^:/)
+              request[:arguments][key] = arg[1..-1].to_sym
+            end
+          end
+        end
+      end
+
+      required_method = required_constant.method(request[:method])
+
+      if request[:arguments].is_a? Hash
+        required_method.call *request[:arguments].values
+      else
+        required_method.call
+      end
     end
 
     def push(connection, response)
